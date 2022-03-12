1612d3a94c807bae2ff26f0b4a0d3e52976f3c7d
Tolulope <adetulatolu@gmail.com>
2022-03-12 12:44:36 +0100

    feat: add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configuration } from 'config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+let envFilePath = `${process.cwd()}/config/env/.env.development`;
+
+console.log(`Running in ${process.env.NODE_ENV}`);
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  envFilePath = '.env';
+}
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath,
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('database.host'),
+          port: +configService.get('database.port'),
+          username: configService.get('database.user'),
+          password: configService.get('database.password'),
+          database: configService.get('database.name'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
